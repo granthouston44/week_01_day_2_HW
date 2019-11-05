1b2afd5b3ab5b6669bdefb6923479bb7950df93f
Grant Houston <grant.houston44@hotmail.com>
2019-11-05 16:37:38 +0000

    all functions defined minus 3

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,3 +30,16 @@ def add_string_as_number(number_one, number_two)
 
   return number_one.to_i + number_two.to_i
 end
+
+def number_to_full_month_name(month_number)
+  case month_number
+  when 1
+      return "January"
+  when 3
+    return "March"
+  when 9
+    return "September"
+  else
+    return "Wrong month number"
+  end
+end
