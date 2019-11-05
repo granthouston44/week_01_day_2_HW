233ff09c1063d209d6edb8d180a742c4967fafa6
Grant Houston <grant.houston44@hotmail.com>
2019-11-05 15:52:31 +0000

    4th and 5th def

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -9,3 +9,11 @@ end
 def subtract(number_one, number_two)
   return number_one.to_i - number_two.to_i
 end
+
+def multiply(number_one, number_two)
+  return number_one.to_i * number_two.to_i
+end
+
+def divide(number_one, number_two)
+  return number_one.to_i / number_two.to_i
+end
