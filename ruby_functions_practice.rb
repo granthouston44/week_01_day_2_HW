3bb2e83f323dc49666a5ea47fe71d4f755e7ce16
Grant Houston <grant.houston44@hotmail.com>
2019-11-05 16:13:20 +0000

    defined up to 7 functions

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -17,3 +17,11 @@ end
 def divide(number_one, number_two)
   return number_one.to_i / number_two.to_i
 end
+
+def length_of_string(test_string)
+  return test_string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
